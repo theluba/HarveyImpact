a50a04fe2136a004dbbfcbf1a9f683a2edb8620b
Luba Guzei <lubaguzei@Lubas-MacBook-Air.local>
2018-04-13 18:35:05 -0400

    Updated map with new color scheme, legend with title, initialize with only block group layer, and display info on geojson click

--- js/script.js
+++ js/script.js
@@ -1,88 +1,50 @@
-// To do
-
-// MAP
-// DONE JUSTIN HELPED figure out how to set up option selection
-// NO NEED FOR THIS I THINK create geojson for zips
-// DONE create geojson for city council 
-// map block groups to city council
-// DONE create geojson 100 and 500 year flood zone
-// create geojson for watersheds
-// buyouts?
-
-
-
-
-
-// DATA 
-// Get data on spending in each waterway on flooding
-// get places that flooded in past 2 floods (may need to pull fema)
-// get data from jeff on places that flooded Harvey
-
-
-// Add blockgroup layer
-// var blockGroup = L.tileLayer(blockGroupJSON),
-
 
 // creates a map centered on Houston, zoomed in to level 10
 var map = L.map('map', {
   center: [29.7604, -95.3698], 
   zoom: 10,
-  // layers: [blockGroup]
 });
 
-
+// adds base reality
 var basicBaseLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibHViYWd1emVpIiwiYSI6ImNqMTd6YmcxOTA2NmUycW83Nnc3bHppMXUifQ.I5MeLgSRNHdBztBDWvQmbA', {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    attribution: ' © Luba Guzei, ' + '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light'
 }).addTo(map);
 
-// This is a layer just to see all the block group outlines without color
-// var geojson = L.geoJson(harris100YrFloodplainJSON).addTo(map);
 
-
-// Set Color
-// function getColor(d) {
-//     return d > 3000 ? '#006400' :
-//            d > 2000 ? '#32CD32' :
-//            d > 1000 ? '#CAFF70' :
-//            d > 500  ? '#E3CF57' :
-//            d > 100  ? '#EE7942' :
-//                       '#EE4000';
+// // this is to see something in a basic blue
+// function style(features) {
+//     return {
+//         fillColor: 'lightblue',
+//         weight: .5,
+//         opacity: 1,
+//         color: 'blue',
+//         // color: 'blue',
+//         fillOpacity: 0.7
+//     };
 // }
 
-// L.geoJSON(layer, {
-// filter: function(feature) {
-// return features.properties.FLD_ZONE === "AE"
-// },
-
-
-function style(features) {
-    return {
-        fillColor: 'lightblue',
-        weight: .5,
-        opacity: 1,
-        color: 'blue',
-        // color: 'blue',
-        fillOpacity: 0.7
-    };
-}
-
+// Choropleth view
 function getColor(d) {
-    return d > 3000 ? 'black' :
-           d > 1000  ? '#99000D' :
-           d > 500  ? '#CB181D' :
-           d > 400  ? '#EF3B2C' :
-           d > 300   ? '#FB6A4A' :
-           d > 200   ? '#FC9272' :
-           d > 100   ? '#FCBBA1' :
-           d > 0 ? '#FEE5D9' :
-                      'white';
+    return d > 30000 ? '#660000' :
+           d > 20000 ? '#cc0000' :
+           d > 10000   ? '#ff6666' :
+           d > 7500   ? '#ff9999' :
+           d > 5000   ? '#db87bd' :
+           d > 2500 ? '#bd78db' :
+           d > 1000  ? '#996bff' :
+           d > 500  ? '#9985ff' :
+           d > 250   ? '#99a3ff' :
+           d > 100   ? '#99bdff' :
+           d > 0 ? ' #cce0ff' :
+                      'transparent';
 }
 
-function styleHarvey(feature) {
+// Choropleth style for Block Group Layer
+function styleHarveyBG(feature) {
     return {
         fillColor: getColor(feature.properties.Harvey_Affected_Units_Total),
         weight: .5,
@@ -93,7 +55,19 @@ function styleHarvey(feature) {
     };
 }
 
+// Choropleth style for CC and SNB Layers
+function styleHarveyCCSNB(feature) {
+    return {
+        fillColor: getColor(feature.properties.HarveyAffectTotalUnits),
+        weight: .5,
+        opacity: 1,
+        color: 'white',
+        // dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 
+// Color blocks by district
 function getColorDist(d) {
     return d == 'A' ? 'black' :
            d == 'B' ? 'lavender' :
@@ -169,9 +143,49 @@ function styleHarveySN(feature) {
 }
 
 
-var blockGroupLayer = L.geoJson(blockGroupJSON, {style: styleHarvey}).addTo(map),
-    cityCouncilLayer = L.geoJson(cityCouncilJSON, {style: style}).addTo(map);
-    superNBLayer = L.geoJson(superNBJSON, {style: styleHarveySN}).addTo(map);
+// // Adds info box on the top
+
+// var info = L.control();
+
+// info.onAdd = function (map) {
+//   this._div = L.DomUtil.create('div', 'info');
+//   this.update();
+//   return this._div;
+// };
+
+// info.update = function (props) {
+//   this._div.innerHTML = '<h4>Estimated Number of Units Affected</h4>' +  (props ?
+//     '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+//     : 'Hover over a geography for more info');
+// };
+
+// info.addTo(map);
+
+var blockGroupLayer = L.geoJson(blockGroupJSON, {style: styleHarveyBG,
+    onEachFeature: function(feature, layer){
+        layer.bindPopup('Units affected:' + feature.properties.Harvey_Affected_Units_Total);
+    }
+}).addTo(map);
+
+var cityCouncilLayer = L.geoJson(cityCouncilJSON, {style: styleHarveyCCSNB,
+    onEachFeature: function(feature, layer){
+        unitsAffected = Math.round(feature.properties.HarveyAffectTotalUnits),
+        // unitsWDecimals = Math.number.toLocaleString(unitsAffected),
+        layer.bindPopup('City Council District: '+ feature.properties.DISTRICT + '<p> Units affected: ' + unitsAffected.toLocaleString());
+    }
+})
+
+var superNBLayer = L.geoJson(superNBJSON, {style: styleHarveyCCSNB,
+    onEachFeature: function(feature, layer){
+        unitsAffected = Math.round(feature.properties.HarveyAffectTotalUnits),
+        // unitsWDecimals = Math.number.toLocaleString(unitsAffected),
+        layer.bindPopup('Super Neighborhood: '+ feature.properties.SNBNAME + '<p> Units affected: ' + unitsAffected.toLocaleString());
+    }
+})
+
+// var blockGroupLayer = L.geoJson(blockGroupJSON, {style: styleHarveyBG}).addTo(map),
+// var cityCouncilLayer = L.geoJson(cityCouncilJSON, {style: styleHarveyCCSNB})
+// var    superNBLayer = L.geoJson(superNBJSON, {style: styleHarveyCCSNB})
     // FEMAnoSUBTYLayer = L.geoJson(FEMAnoSUBTYjson, {style: style}).addTo(map);
     // FEMA500YrFEMALayer = L.geoJson(FEMA500YrFEMAjson, {style: style}).addTo(map);
     // FEMAminhazardLayer = L.geoJson(FEMAminhazardjson, {style: style}).addTo(map);
@@ -192,8 +206,8 @@ var blockGroupLayer = L.geoJson(blockGroupJSON, {style: styleHarvey}).addTo(map)
 
 var overlayMaps = {
     "Block Groups": blockGroupLayer,
-    "City Council Districts": cityCouncilLayer,
-    "Super Neighborhoods": superNBLayer
+    "Super Neighborhoods": superNBLayer,
+    "City Council Districts": cityCouncilLayer
     // "FEMA no subty" : FEMAnoSUBTYLayer, 
     // "FEMA 500 Yr" : FEMA500YrFEMALayer, 
     // "FEMA In Hazard" : FEMAminhazardLayer, 
@@ -209,31 +223,124 @@ var overlayMaps = {
 
 L.control.layers(null, overlayMaps).addTo(map);
 
-// ***********
 
-// Everything below this is not relevant
+// // ************
+// // Add mouseover
+// // ************
+
+
+//   // control that shows state info on hover
+//   var info = L.control();
+
+//   info.onAdd = function (map) {
+//     this._div = L.DomUtil.create('div', 'info');
+//     this.update();
+//     return this._div;
+//   };
+
+//   info.update = function (props) {
+//     this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+//       '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+//       : 'Hover over a state');
+//   };
+
+//   info.addTo(map);
+
+
+//   // get color depending on population density value
+//   function getColor(d) {
+//     return d > 1000 ? '#800026' :
+//         d > 500  ? '#BD0026' :
+//         d > 200  ? '#E31A1C' :
+//         d > 100  ? '#FC4E2A' :
+//         d > 50   ? '#FD8D3C' :
+//         d > 20   ? '#FEB24C' :
+//         d > 10   ? '#FED976' :
+//               '#FFEDA0';
+//   }
+
+//   function style(feature) {
+//     return {
+//       weight: 2,
+//       opacity: 1,
+//       color: 'white',
+//       dashArray: '3',
+//       fillOpacity: 0.7,
+//       fillColor: getColor(feature.properties.density)
+//     };
+//   }
+
+//   function highlightFeature(e) {
+//     var layer = e.target;
+
+//     layer.setStyle({
+//       weight: 5,
+//       color: '#666',
+//       dashArray: '',
+//       fillOpacity: 0.7
+//     });
+
+//     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+//       layer.bringToFront();
+//     }
+
+//     info.update(layer.feature.properties);
+//   }
+
+//   var geojson;
+
+//   function resetHighlight(e) {
+//     geojson.resetStyle(e.target);
+//     info.update();
+//   }
+
+//   function zoomToFeature(e) {
+//     map.fitBounds(e.target.getBounds());
+//   }
+
+//   function onEachFeature(feature, layer) {
+//     layer.on({
+//       mouseover: highlightFeature,
+//       mouseout: resetHighlight,
+//       click: zoomToFeature
+//     });
+//   }
+
+//   geojson = L.geoJson(statesData, {
+//     style: style,
+//     onEachFeature: onEachFeature
+//   }).addTo(map);
+
+
+
 
 // ************
 // Add legend to the map
-// var legend = L.control({position: 'bottomright'});
+// ************
+var legend = L.control({position: 'bottomright'});
 
-// legend.onAdd = function (map) {
+legend.onAdd = function (map) {
 
-//     var div = L.DomUtil.create('div', 'info legend'),
-//         grades = [0, 100, 500, 1000, 2000, 3000],
-//         labels = [];
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 100, 250, 500, 1000, 2500, 5000, 7500, 10000, 20000, 30000],
+        labels = ["<strong> Units Affected (estimated)</strong>"],
+        // labels = [],
+        from, to;
 
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < grades.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-//     }
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+            to = grades[i + 1];
 
-//     return div;
-// };
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+  div.innerHTML = labels.join('<br>');
+  return div;
+};
 
-// legend.addTo(map);
+legend.addTo(map);
 
 
 // // ************
